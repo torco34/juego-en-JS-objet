173d207cb484c81482b03269981c0fa156326f51
torco34 <torcoromaarias@gimail.com>
2020-06-18 02:01:52 -0500

    segundo commit

---
+++ folder.js/index.js
@@ -0,0 +1,36 @@
+let span = document.getElementById('blackDiv')
+
+function black(){
+  span.setAttribute('id', 'blackDiv');
+}
+
+function yellow(){
+  span.setAttribute('id', 'yellowDiv');
+}
+
+function red(){
+  span.setAttribute('id', 'redDiv');
+}
+function greed(){
+  span.setAttribute('id', 'greedDiv');
+}
+
+function morado(){
+	fondo.style.background="#56107A";
+}
+function rosado(){
+	fondo.style.background="#EF7ECA";
+}
+function ouscuro(){
+	fondo.style.background="#480C35";
+}
+function verdecito(){
+	fondo.style.background="#18ABA5";
+}
+function verde(){
+	fondo.style.background="#75AB18";
+}
+
+function naranja(){
+	fondo.style.background="#D77511";
+}
