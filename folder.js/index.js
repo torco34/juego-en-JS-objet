12cb811f36df729856cd6d9609ee04d541204419
torco34 <torcoromaarias@gimail.com>
2020-08-25 12:17:54 -0500

    Primer commit

--- folder.js/index.js
+++ folder.js/index.js
@@ -1,48 +1,82 @@
-let cuadroUno = document.getElementById('cuadro1')
-let cuadroDos = document.getElementById('cuadro2')
-let cuadroTres = document.getElementById('cuadro3')
-let cuadroCuatro = document.getElementById('cuadro4')
-let cuadroCinco = document.getElementById('cuadro5')
-let cuadroSeis = document.getElementById('cuadro6')
-let cuadroSiete= document.getElementById('cuadro7')
-let cuadroOcho = document.getElementById('cuadro8')
-let cuadroNueve = document.getElementById('cuadro9')
-let cuadroDiez = document.getElementById('cuadro10')
-let cuadroOnce= document.getElementById('cuadro11')
-let cuadroDoce = document.getElementById('cuadro12')
-let cuadroTrece = document.getElementById('cuadro13')
-let cuadroCatorce = document.getElementById('cuadro14')
+const celeste = document.getElementById( 'celeste');
+const violeta = document.getElementById('violeta');
+const naranja = document.getElementById('naranja');
+const verde  = document.getElementById('verde');
 
-const comenzoJuego = document.getElementById('iniciar_juego')
-console.log(comenzoJuego)
+const botonEmpezar = document.getElementById('btnEmpezar');
 
+// console.log(botonE)
 
 class Juego {
-
-
 	constructor(){
-		this.iniciar()
-		this.secuenciaBotones()
+		this.inicializarJuego()
+		this.generarSecuencia()
+		this.siguienteNivel()
 	}
-	iniciar(){
-		comenzoJuego.classList.add('borrar')
+	inicializarJuego(){
+		this.eligirColor = this.eligirColor.bind(this)
+		botonEmpezar.classList.add('hide')
 		this.nivel = 1
 		this.colores = {
-			rosa: cuadroUno,
-			celeste:  cuadroTres,
-			morado: cuadroCinco,
-			azul:  cuadroSiete,
-			verde:cuadroNueve,
-			trullo: cuadroOnce,
-			cian:cuadroOnce
+			celeste,
+			violeta,
+			naranja,
+			verde
 		}
 
 	}
-	secuenciaBotones(){
+	generarSecuencia(){
 		this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4))
+	}
+
+	siguienteNivel(){
+    this.iluminarSecuencia()
+    this.agregarEventosClick()
+	}
+
+	tranformarNumeroAColor(numero){
+		switch (numero){
+			case 0:
+			return 'celeste'
+			case 1:
+			return 'violeta'
+			case 2:
+			return 'naranja'
+			case 3:
+			return 'verde'
+		}
+
+	}
+	iluminarSecuencia(){
+		for(let i = 0; i<this.nivel; i++){
+			const color = this.tranformarNumeroAColor(this.secuencia[i])
+            setTimeout(() => this.iluminarColor(color), 1000 * i)
+		}
+	}
+	iluminarColor(color){
+		
+		this.colores[color].classList.add('light')
+		
+		setTimeout(() => this.apagarColor(color), 350)
+	}
+	apagarColor(color){
+		this.colores[color].classList.remove('light')
+	}
+	agregarEventosClick(){
+		this.colores.celeste.addEventListener('click', this.eligirColor)
+		this.colores.verde.addEventListener('click', this.eligirColor)
+		this.colores.violeta.addEventListener('click', this.eligirColor)
+		this.colores.naranja.addEventListener('click', this.eligirColor)
+
+	}
+	eligirColor(ev){
+		console.log(this)
+
 	}
 }
 
-function iniciaJuego(){
-window.juego = new Juego()
+function empezarJuego(){
+	window.juego = new Juego()
+
 }
+// alert('hola')
